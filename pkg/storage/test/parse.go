b58a11bf87172236c0fdaded47d47ecbee0292cc
Mike Montague <mikemon@gmail.com>
2024-06-23 10:17:25 -0700

    storage: test: parse value

---
+++ pkg/storage/test/parse.go
@@ -0,0 +1,177 @@
+package test
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+	"unicode"
+
+	"github.com/leftmike/maho/pkg/types"
+)
+
+var (
+	errUnexpectedEOF  = errors.New("parse: unexpected eof")
+	errBadBytesString = errors.New("parse: bad bytes string")
+)
+
+func skipWhitespace(rs io.RuneScanner) (rune, error) {
+	for {
+		r, _, err := rs.ReadRune()
+		if err != nil {
+			return 0, err
+		}
+		if !unicode.IsSpace(r) {
+			return r, nil
+		}
+	}
+}
+
+func readRunes(rs io.RuneScanner, r rune, eofError bool, pred func(rune) bool) (string, error) {
+	var buf strings.Builder
+	if r != 0 {
+		buf.WriteRune(r)
+	}
+
+	for {
+		r, _, err := rs.ReadRune()
+		if err == io.EOF {
+			if eofError {
+				return "", errUnexpectedEOF
+			}
+			break
+		} else if err != nil {
+			return "", errUnexpectedEOF
+		}
+
+		if pred(r) {
+			buf.WriteRune(r)
+		} else {
+			rs.UnreadRune()
+			break
+		}
+	}
+
+	return buf.String(), nil
+}
+
+func hexDigit(ch byte) byte {
+	if ch >= '0' && ch <= '9' {
+		return ch - '0'
+	} else if ch >= 'a' && ch <= 'f' {
+		return ch - 'a' + 10
+	}
+	return ch - 'A' + 10
+}
+
+func ParseValue(rs io.RuneScanner) (types.Value, error) {
+	r, err := skipWhitespace(rs)
+	if err != nil {
+		return nil, err
+	}
+
+	if unicode.IsDigit(r) || r == '.' {
+		s, err := readRunes(rs, r, false,
+			func(r rune) bool {
+				return unicode.IsDigit(r) || r == '.'
+			})
+		if err != nil {
+			return nil, err
+		}
+
+		if i, err := strconv.ParseInt(s, 10, 64); err == nil {
+			return types.Int64Value(i), nil
+		}
+
+		if f, err := strconv.ParseFloat(s, 64); err == nil {
+			return types.Float64Value(f), nil
+		}
+		return nil, fmt.Errorf("parse: expected a number: %s", s)
+	} else if unicode.IsLetter(r) {
+		s, err := readRunes(rs, r, false,
+			func(r rune) bool {
+				return unicode.IsLetter(r)
+			})
+		if err != nil {
+			return nil, err
+		}
+
+		s = strings.ToLower(s)
+		switch s {
+		case "true":
+			return types.BoolValue(true), nil
+		case "false":
+			return types.BoolValue(false), nil
+		case "null":
+			return nil, nil
+		default:
+			return nil, fmt.Errorf("parse: unexpected identifier: %s", s)
+		}
+	} else if r != '\'' {
+		return nil, fmt.Errorf("parse: unexpected rune parsing value: %v %d", r, r)
+	}
+
+	r, _, err = rs.ReadRune()
+	if err == io.EOF {
+		return nil, errUnexpectedEOF
+	} else if err != nil {
+		return nil, err
+	}
+
+	if r == '\\' {
+		r, _, err = rs.ReadRune()
+		if err == io.EOF {
+			return nil, errUnexpectedEOF
+		} else if err != nil {
+			return nil, err
+		} else if r != 'x' && r != 'X' {
+			return nil, errBadBytesString
+		}
+
+		s, err := readRunes(rs, 0, true,
+			func(r rune) bool {
+				return r != '\''
+			})
+		if err != nil {
+			return nil, err
+		}
+		rs.ReadRune()
+
+		if len(s)%2 != 0 {
+			return nil, errBadBytesString
+		}
+
+		b := make([]byte, len(s)/2)
+		for idx := 0; idx < len(s); idx += 2 {
+			b[idx/2] = (hexDigit(s[idx]) << 4) | hexDigit(s[idx+1])
+		}
+
+		return types.BytesValue(b), nil
+	} else if r == '\'' {
+		return types.StringValue(""), nil
+	}
+
+	s, err := readRunes(rs, r, true,
+		func(r rune) bool {
+			return r != '\''
+		})
+	if err != nil {
+		return nil, err
+	}
+	rs.ReadRune()
+
+	return types.StringValue(s), nil
+}
+
+func ParseRow(rs io.RuneScanner) (types.Row, error) {
+	// (123, 'abc', true, 456.789, '\x010203')
+	// XXX
+	return nil, nil
+}
+
+func ParseRows(rs io.RuneScanner) ([]types.Row, error) {
+	// (123, 'abc', true), (456, 'def', false), (789, 'ghi', null)
+	// XXX
+	return nil, nil
+}
---
+++ pkg/storage/test/parse_test.go
@@ -0,0 +1,52 @@
+package test_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/leftmike/maho/pkg/storage/test"
+	"github.com/leftmike/maho/pkg/types"
+)
+
+func TestParseValue(t *testing.T) {
+	cases := []struct {
+		s    string
+		val  types.Value
+		fail bool
+	}{
+		{s: "'abc'", val: types.StringValue("abc")},
+		{s: " \t\n 'abc'  ", val: types.StringValue("abc")},
+		{s: "'", fail: true},
+		{s: "' ", fail: true},
+		{s: "123", val: types.Int64Value(123)},
+		{s: " 123 ", val: types.Int64Value(123)},
+		{s: "123.456", val: types.Float64Value(123.456)},
+		{s: "123..456", fail: true},
+		{s: ".", fail: true},
+		{s: ".123", val: types.Float64Value(0.123)},
+		{s: "true", val: types.BoolValue(true)},
+		{s: "  False  ", val: types.BoolValue(false)},
+		{s: "t", fail: true},
+		{s: "null", val: nil},
+		{s: `'\a'`, fail: true},
+		{s: `'\x1234`, fail: true},
+		{s: `'\x123'`, fail: true},
+		{
+			s:   `'\x1234567890abcdef'`,
+			val: types.BytesValue([]byte{0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xcd, 0xEf}),
+		},
+	}
+
+	for _, c := range cases {
+		val, err := test.ParseValue(strings.NewReader(c.s))
+		if err != nil {
+			if !c.fail {
+				t.Errorf("ParseValue(%s) failed with %s", c.s, err)
+			}
+		} else if c.fail {
+			t.Errorf("ParseValue(%s) did not fail", c.s)
+		} else if types.Compare(val, c.val) != 0 {
+			t.Errorf("ParseValue(%s) got %s want %s", c.s, val, c.val)
+		}
+	}
+}
